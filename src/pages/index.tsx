b418afe2a858458e8c3d8782cb967e22f5102a50
waldo <luis.betancourt.dev@gmail.com>
2023-04-28 12:47:45 -0500

    Post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-
 import { api } from "~/utils/api";
 
+// import { api } from "~/utils/api";
+
 const Home: NextPage = () => {
-    const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
     const user = useUser();
+
+    const { data } = api.posts.getAll.useQuery();
 
     return (
         <>
@@ -18,9 +19,14 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-                <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-                {!user.isSignedIn && <SignInButton /> }
-                {!!user.isSignedIn && <SignOutButton /> }
+                <div>
+                    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+                    {!user.isSignedIn && <SignInButton /> }
+                    {!!user.isSignedIn && <SignOutButton /> }
+                </div>
+                <div>
+                    {data?.map(post => (<div key={post.id}>{post.content}</div>))}
+                </div>
             </main>
         </>
     );
